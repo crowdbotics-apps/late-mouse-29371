619a95f9aea0a257809df88dbcc3c7c04dec5fa2
Crowdbotics <team@crowdbotics.com>
2021-08-03 03:42:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "late_mouse_29371Identifier",
+  url: "https://late_mouse_29371Identifier.botics.co",
+  api: "https://late_mouse_29371Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
